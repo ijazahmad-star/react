b0fc737576d8454cb4d75dba4d108d64b29f5d14
ijazahmad-star <ijaz.ahmad@strategisthub.com>
2025-11-10 12:44:50 +0500

    Context-api and Reducer hook

---
+++ context-api_and_Reducers/src/App.jsx
@@ -0,0 +1,23 @@
+import Header from "./components/Header.jsx";
+import Shop from "./components/Shop.jsx";
+import Product from "./components/Product.jsx";
+import { DUMMY_PRODUCTS } from "./dummy-products.js";
+
+import ContextProvider from "./store/shopping-cart-context.jsx";
+
+function App() {
+  return (
+    <ContextProvider>
+      <Header />
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} />
+          </li>
+        ))}
+      </Shop>
+    </ContextProvider>
+  );
+}
+
+export default App;
