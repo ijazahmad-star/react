3fb2d3111eaac9cc1595731288e4ba79065da50e
ijazahmad-star <ijaz.ahmad@strategisthub.com>
2025-11-11 17:26:12 +0500

    updated fetch data from endpoint using custom hooks

--- 10-ConnectDatabase/src/App.jsx
+++ 10-ConnectDatabase/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useCallback, useEffect } from "react";
+import { useRef, useState, useCallback } from "react";
 
 import Places from "./components/Places.jsx";
 import Modal from "./components/Modal.jsx";
@@ -8,34 +8,18 @@ import AvailablePlaces from "./components/AvailablePlaces.jsx";
 import Error from "./components/Error.jsx";
 import { fetchUserPlaces, updateUserPlaces } from "./http.js";
 import ErrorPage from "./components/Error.jsx";
+import { useFetch } from "./customHooks/useFetch.js";
 function App() {
   const selectedPlace = useRef();
-
-  const [userPlaces, setUserPlaces] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState();
-
   const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
-
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-      try {
-        const places = await fetchUserPlaces();
-        setUserPlaces(places);
-      } catch (error) {
-        setError({
-          message: error.message || "Failed to fetch user places",
-        });
-      }
-
-      setIsFetching(false);
-    }
-
-    fetchPlaces();
-  }, []);
+  const {
+    isFetching,
+    error,
+    fetchedData: userPlaces,
+    setFetchedData:setUserPlaces
+  } = useFetch(fetchUserPlaces, []);
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -86,7 +70,7 @@ function App() {
 
       setModalIsOpen(false);
     },
-    [userPlaces]
+    [userPlaces, setUserPlaces]
   );
 
   function handleError() {
@@ -119,7 +103,9 @@ function App() {
         </p>
       </header>
       <main>
-        {error && <ErrorPage title="An error occured" message={error.message}/>}
+        {error && (
+          <ErrorPage title="An error occured" message={error.message} />
+        )}
         {!error && (
           <Places
             title="I'd like to visit ..."
@@ -131,7 +117,9 @@ function App() {
           />
         )}
 
-        <AvailablePlaces onSelectPlace={handleSelectPlace} />
+        <AvailablePlaces
+        onSelectPlace={handleSelectPlace}
+        />
       </main>
     </>
   );
--- 10-ConnectDatabase/src/components/AvailablePlaces.jsx
+++ 10-ConnectDatabase/src/components/AvailablePlaces.jsx
@@ -1,52 +1,33 @@
-import { useState, useEffect } from "react";
-
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
 
 import { sortPlacesByDistance } from "../loc.js";
-import {fetchAvailablePlaces} from '../http.js'
+import { fetchAvailablePlaces } from "../http.js";
+import { useFetch } from "../customHooks/useFetch.js";
+
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      resolve(sortedPlaces)
+    });
+  });
+}
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isFetching, setIsFetching] = useState(false);
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [error, setError] = useState();
+
 
   // rest of code...
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-      try {
-        // const response = await fetch("http://localhost:3000/places");
-        // const resData = await response.json();
-
-        // if (!response.ok) {
-        //   throw new Error("Failed to fetch places");
-        // }
-        const places = await fetchAvailablePlaces()
-        navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(
-            places,
-            position.coords.latitude,
-            position.coords.longitude
-          );
-          setAvailablePlaces(sortedPlaces);
-          setIsFetching(false);
-        });
-
-        // setAvailablePlaces(resData.places);
-      } catch (error) {
-        setError({
-          message:
-            error.message || "Could not fetch places, please try again later!",
-        });
-        setIsFetching(false);
-      }
-      // setIsFetching(false);
-    }
-
-    fetchPlaces();
-  }, []);
-
+  const {
+    isFetching,
+    error,
+    fetchedData: availablePlaces,
+  } = useFetch(fetchSortedPlaces, []);
   if (error) {
     return <Error title="An Error occured" message={error.message} />;
   }
